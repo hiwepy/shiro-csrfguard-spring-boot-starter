0e75d07e8503770090e3bf51b139f04e97e8cf24
wandl <hiwepy@163.com>
2026-05-27 11:26:27 +0800

    chore(pom): align to Spring Boot 3.0.x (JDK 17, Boot 3.0.13)

--- src/main/java/org/apache/shiro/spring/boot/csrfguard/web/filter/CsrfGuardControlFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/csrfguard/web/filter/CsrfGuardControlFilter.java
@@ -19,9 +19,9 @@ import org.apache.shiro.web.filter.AccessControlFilter;
 import org.owasp.csrfguard.CsrfGuard;
 import org.owasp.csrfguard.CsrfGuardFilter;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**                
