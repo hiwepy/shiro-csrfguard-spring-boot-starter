ba5322c52d372f6debdb688c986fa3c2ebff82ef
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:54:43 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 2.3.x branch

--- src/main/java/org/apache/shiro/spring/boot/csrfguard/web/filter/CsrfGuardControlFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/csrfguard/web/filter/CsrfGuardControlFilter.java
@@ -19,14 +19,14 @@ import org.apache.shiro.web.filter.AccessControlFilter;
 import org.owasp.csrfguard.CsrfGuard;
 import org.owasp.csrfguard.CsrfGuardFilter;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**                
  * 拷贝 org.owasp.csrfguard.CsrfGuardFilter
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class CsrfGuardControlFilter extends AccessControlFilter {
 
