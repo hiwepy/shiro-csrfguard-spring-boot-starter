4d60cbd234405d08b1454341b21cf7ff3e0358e3
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:55:20 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.4.x branch

--- src/main/java/org/apache/shiro/spring/boot/csrfguard/CsrfguardConstants.java
+++ src/main/java/org/apache/shiro/spring/boot/csrfguard/CsrfguardConstants.java
@@ -17,7 +17,7 @@ package org.apache.shiro.spring.boot.csrfguard;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class CsrfguardConstants {
 
